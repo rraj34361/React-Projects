6f4f13cc1006e18f29234b1a08d1dd70839bc51a
Ravi <rraj34361@gmail.com>
2023-06-10 10:14:12 +0530

    working

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,11 @@
+import {useContext} from 'react'
 import React from 'react'
 import BookShow from './BookShow'
+import BooksContext from '../Context/books'
 export default function BookList({books , onDelete, onEdit}) {
+     
+
+
     const renderedBooks = books.map((book)=>{
    return <BookShow key = {book.id} onEdit = {onEdit} onDelete = {onDelete} book = {book}/>
     })
@@ -8,6 +13,7 @@ export default function BookList({books , onDelete, onEdit}) {
 
   return (
     <div className='book-list'>
+  
         {renderedBooks} 
     </div>
   )
